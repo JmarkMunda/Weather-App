ef005f1d5c4f7da24452c89fa90d9ddfe9e2e8e7
poopijay <jaymarkmunda26@gmail.com>
2022-07-12 18:35:22 +0800

    add default location

--- src/App.js
+++ src/App.js
@@ -5,12 +5,25 @@ import Header from './components/Header';
 import Content from './components/Content';
 
 import {raining} from './assets/raining.jpg';
+import { useEffect } from 'react';
 
 function App() {
   const [data, setData] = useState({});
-  const [location, setLocation] = useState('');
+  const [location, setLocation] = useState('Manila');
 
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=bdd58312886433b5f371378b047bad2f`;
+
+  useEffect(() => {
+    const fetchLocation = async() => {
+      await axios.get(url)
+      .then((response) => {
+      setData(response.data)
+      })
+    }
+    fetchLocation();
+    setLocation('')
+  }, [])
+
 
   const searchLocation = (e) => {
     if (e.key === 'Enter') {
